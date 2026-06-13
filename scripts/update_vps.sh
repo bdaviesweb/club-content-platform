99cf05d331cd670b5278ae8961a15a21d6ee8a54
bdaviesweb <93673519+bdaviesweb@users.noreply.github.com>
2026-06-12 22:28:23 -0400

    Add detached VPS update mode

--- scripts/update_vps.sh
+++ scripts/update_vps.sh
@@ -7,6 +7,20 @@ RUN_APPROVAL_PUBLISH_SMOKE="${RUN_APPROVAL_PUBLISH_SMOKE:-0}"
 SMOKE_TIMEOUT_SECONDS="${SMOKE_TIMEOUT_SECONDS:-300}"
 HEALTH_TIMEOUT_SECONDS="${HEALTH_TIMEOUT_SECONDS:-60}"
 HEALTH_POLL_SECONDS="${HEALTH_POLL_SECONDS:-2}"
+DETACH="${DETACH:-0}"
 
-ssh "${REMOTE_HOST}" \
-  "REPO_DIR='${REMOTE_DIR}' RUN_APPROVAL_PUBLISH_SMOKE='${RUN_APPROVAL_PUBLISH_SMOKE}' SMOKE_TIMEOUT_SECONDS='${SMOKE_TIMEOUT_SECONDS}' HEALTH_TIMEOUT_SECONDS='${HEALTH_TIMEOUT_SECONDS}' HEALTH_POLL_SECONDS='${HEALTH_POLL_SECONDS}' bash '${REMOTE_DIR}/scripts/pull_and_restart_vps.sh'"
+remote_env="REPO_DIR='${REMOTE_DIR}' RUN_APPROVAL_PUBLISH_SMOKE='${RUN_APPROVAL_PUBLISH_SMOKE}' SMOKE_TIMEOUT_SECONDS='${SMOKE_TIMEOUT_SECONDS}' HEALTH_TIMEOUT_SECONDS='${HEALTH_TIMEOUT_SECONDS}' HEALTH_POLL_SECONDS='${HEALTH_POLL_SECONDS}'"
+remote_script="'${REMOTE_DIR}/scripts/pull_and_restart_vps.sh'"
+
+if [ "${DETACH}" = "1" ]; then
+  ssh "${REMOTE_HOST}" "
+    cd '${REMOTE_DIR}'
+    log=\"/tmp/club-content-update-\$(date -u +%Y%m%dT%H%M%SZ).log\"
+    nohup env ${remote_env} bash ${remote_script} </dev/null >\"\${log}\" 2>&1 &
+    echo \"update_log=\${log}\"
+    echo \"tail_command=ssh ${REMOTE_HOST} 'tail -f \${log}'\"
+  "
+  exit 0
+fi
+
+ssh "${REMOTE_HOST}" "${remote_env} bash ${remote_script}"
